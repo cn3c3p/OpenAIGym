55081dbc1a96c9faf308da65bfece13e4ee61fc3
Jacob Chen <chen.jacob92@gmail.com>
2017-02-01 20:42:22 -0800

    Actor Critic annealing move to add experience Tuple

---
+++ LunarLander-v2/Q_Learner.py
@@ -0,0 +1,52 @@
+import time
+from keras.layers import Input
+from keras.layers.core import Dense
+from keras.models import Model
+
+import math
+import numpy as np
+from random import shuffle
+import pickle as pickle
+
+import sys
+maj = sys.version_info
+version = 2
+
+if maj[0] >= 3:
+	import _pickle as pickle
+	import importlib.machinery
+	import types
+	version = 3
+else:
+	import cPickle as pickle
+	import imp
+
+if version == 3:
+	loader = importlib.machinery.SourceFileLoader('Q_Learning', '../RLControllers/Q_Learning.py')
+	module = types.ModuleType(loader.name)
+	loader.exec_module(module)
+
+else:
+	module = imp.load_source('Q_Learning', '../RLControllers/Q_Learning.py')
+
+class Network(module.Network):
+
+	def __init__(self, dense_layers, num_features, num_actions):
+		module.Network.__init__(self,
+								dense_layers,
+								num_features,
+								num_actions,
+								activation='linear',
+								loss='mse',
+								update_num=10,
+								batch_size=320,
+								discount_factor=0.9,
+								exploration=1.0,
+								experience_length=40000,
+								max_iter=100000)
+
+	def explore_action(self, action_space):
+		return np.random.choice(range(0, action_space.n))
+
+	def action_from_Q_values(self, q_values):
+		return np.argmax(q_values)
